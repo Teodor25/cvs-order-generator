4039bc967f0b669ad2071b618589983f82414249
Teodor25 <teodor_jonasson@hotmail.com>
2022-11-17 16:30:50 +0100

    feat: 🎸 more detail to city selection

--- src/customers.py
+++ src/customers.py
@@ -11,12 +11,16 @@ cities = fullList[1]
 def generate_customer():
     cityIndex = 0; 
     number = random.randint(0, 1000);
-    if number < 300: 
+    if number < 350: 
         cityIndex = random.randint(0, 8);
-    elif (number > 300) and (number < 450):
+    elif (number > 350) and (number < 550):
         cityIndex = random.randint(42, 46);
-    elif (number > 450) and (number < 520):
+    elif (number > 550) and (number < 680):
         cityIndex = 53
+    elif (number > 680) and (number < 730):
+        cityIndex = 50
+    elif (number > 730) and (number < 800):
+        cityIndex = random.randint(21, 24);
     else:
         cityIndex = random.randint(0, len(postalCodes) - 1);
 
